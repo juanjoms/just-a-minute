0dd0caede5dac39c7224a7b8f855fa556f1ca43d
Juan Miramontes <juan.miramontes@accenture.com>
2020-01-17 20:50:20 -0600

    First commit :tada:

---
+++ main.js
@@ -0,0 +1,22 @@
+let seconds = 60;
+let timer;
+
+const byId = (sel) => document.getElementById(sel);
+
+const start = () => {
+	byId('start-screen').style.display = 'none';
+	byId('counter-screen').style.display = 'block';
+	byId('time').textContent = seconds;
+	timer = setInterval(updateTime, 1000);
+}
+
+const stop = () => {
+	clearInterval(timer);
+	byId('start-screen').style.display = 'block';
+	byId('counter-screen').style.display = 'none';
+}
+
+const updateTime = () => {
+	byId('time').textContent = --seconds;
+	console.log(seconds);
+}
