1c9290ccc08fc886c22b21cf251a94c65ea928a8
Juan Miramontes <juan.miramontes@accenture.com>
2020-01-19 18:24:52 -0600

    Added manifest.json

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ let timer;
 let stopColor;
 let timeElement;
 
-const byId = (sel) => document.getElementById(sel);
+const elem = (id) => document.getElementById(id);
 
 const start = () => {
   seconds = 60;
-  byId('start-screen').style.display = 'none';
-  byId('counter-screen').style.display = 'block';
-  timeElement = byId('time');
+  elem('start-screen').style.display = 'none';
+  elem('counter-screen').style.display = 'block';
+  timeElement = elem('time');
   timeElement.textContent = seconds;
   timer = setInterval(updateTime, 1000);
   stopColor = getComputedStyle(document.body).getPropertyValue('--stop-color');
@@ -18,14 +18,14 @@ const start = () => {
 const updateTime = () => {
   timeElement.textContent = --seconds;
   if (seconds === 0) {
-    byId('sound').play();
+    elem('sound').play();
     timeElement.style.color = stopColor;
   }
 }
 
 const stop = () => {
-  byId('start-screen').style.display = 'block';
-  byId('counter-screen').style.display = 'none';
+  elem('start-screen').style.display = 'block';
+  elem('counter-screen').style.display = 'none';
   timeElement.removeAttribute('style');
   clearInterval(timer);
 }
